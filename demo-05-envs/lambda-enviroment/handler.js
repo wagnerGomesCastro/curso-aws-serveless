1110acc2f810675fc719dbc725245a06137c3972
Wagner Castro <wagner.castro@m2center.com.br>
2021-08-05 11:27:51 -0300

    #06 16-Definindo projeto em ambientes (part 1)

---
+++ demo-05-envs/lambda-enviroment/handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.scheduler = async(event) => {
+  console.log('***env', process.env)
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+        message: 'Go Serverless v2.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
