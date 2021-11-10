b3e20a667281de1ee2dce520c38555c0b068700a
Wagner Castro <wagner_gcastro@hotmail.com>
2021-11-10 02:19:19 -0300

    #08 [07] -  Processando arquivos sob demanda com Node.js Streams
    #08 [07] -  Processando arquivos sob demanda com Node.js Streams 
    
    [20] - Docker, LocalStack, Mocha, Serverless offline, Nodemon e Mais  [part2]

--- demo-07-tooling/tooling-s3-sqs/handler.js
+++ demo-07-tooling/tooling-s3-sqs/handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Heelo 3',
+        message: 'Heelo 4',
         // input: event,
       },
       null,
